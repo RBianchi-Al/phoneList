1e0a81f25407c91ebbb2e52debb00d61b19d3c79
RBianchi-Al <renatalebianchi@gmail.com>
2021-07-21 00:54:05 -0300

    final

--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -74,20 +74,7 @@ export default function Login() {
               onClick={handleLogin}
             >
               Entrar
-            </Button>
-            {/* <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid> */}
-            
+            </Button>           
           </form>
         </div>
       </Grid>
